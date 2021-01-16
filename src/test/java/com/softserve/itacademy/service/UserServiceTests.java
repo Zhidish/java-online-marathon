1f8baac328c299cf6c0528c88a66f67ed7a4d8c8
PabloJuice <pawlo.dzhus@gmail.com>
2021-01-16 23:48:49 +0200

    release

--- src/test/java/com/softserve/itacademy/service/UserServiceTests.java
+++ src/test/java/com/softserve/itacademy/service/UserServiceTests.java
@@ -15,8 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -31,47 +30,78 @@ public class UserServiceTests {
     @Autowired
     UserRepository userRepository;
 
-//    @Test
-//    @Transactional
-//    @DirtiesContext
-//    public void checkCreateUser(){
-//        User user = new User();
-//        user.setName("Createuser");
-//        user = userService.create(user);
-//        assertTrue(userRepository.existsById(user.getId()));
-//    }
-//    @Test
-//    @Transactional
-//    @DirtiesContext
-//    public void checkReadUser(){
-//        User user = new User();
-//        user.setName("Readuser");
-//        user = userService.create(user);
-//        assertEquals(user, userService.readById(user.getId()));
-//    }
-//    @Test
-//    @Transactional
-//    @DirtiesContext
-//    public void checkUpdateUser(){
-//        User user1 = new User();
-//        User user2;
-//        user1.setName("Updateeuser");
-//        user1 = userService.create(user1);
-//        user1.setName("Updateeuser1");
-//        user2 = userService.update(user1);
-//        assertEquals(user1, user2);
-//        assertEquals(user1.getName(), user2.getName());
-//    }
-//    @Test
-//    @Transactional
-//    @DirtiesContext
-//    public void checkDeleteUser(){
-//        User user = new User();
-//        user.setName("Readuser");
-//        user = userService.create(user);
-//        userService.delete(user.getId());
-//        assertNull(userService.readById(user.getId()));
-//    }
+    @Test
+    @Transactional
+    @DirtiesContext
+    public void checkCreateUser(){
+        User user = new User();
+        user.setEmail("valid2@gmail.ua");
+        user.setFirstName("Create");
+        user.setLastName("User");
+        user.setPassword("qwQW12!@");
+        Role role = new Role();
+        role.setName("UserRole1");
+        role = roleService.create(role);
+        user.setRole(role);
+        user = userService.create(user);
+        assertTrue(userRepository.existsById(user.getId()));
+    }
+    @Test
+    @Transactional
+    @DirtiesContext
+    public void checkReadUser(){
+        User user = new User();
+        user.setEmail("valid2@gmail.ua");
+        user.setFirstName("Create");
+        user.setLastName("User");
+        user.setPassword("qwQW12!@");
+        Role role = new Role();
+        role.setName("UserRole1");
+        role = roleService.create(role);
+        user.setRole(role);
+        user = userService.create(user);
+        assertEquals(user, userService.readById(user.getId()));
+    }
+    @Test
+    @Transactional
+    @DirtiesContext
+    public void checkUpdateUser(){
+        Role role = new Role();
+        role.setName("UserRole1");
+        role = roleService.create(role);
+
+        User user1 = new User();
+        User user2;
+        user1.setFirstName("Updateeuser");
+        user1.setLastName("Userghgh");
+        user1.setPassword("qwQW12!@");
+        user1.setId(56);
+        user1.setRole(role);
+        user1.setEmail("valid2@gmail.ua");
+        user1 = userService.create(user1);
+        user1.setFirstName("Updateeuser1");
+        user2 = userService.update(user1);
+        assertEquals(user1, user2);
+        assertEquals(user1.getFirstName(), user2.getFirstName());
+    }
+
+    @Test
+    @Transactional
+    @DirtiesContext
+    public void checkDeleteUser(){
+        User user = new User();
+        user.setEmail("valid2@gmail.ua");
+        user.setFirstName("Create");
+        user.setLastName("User");
+        user.setPassword("qwQW12!@");
+        Role role = new Role();
+        role.setName("UserRole1");
+        role = roleService.create(role);
+        user.setRole(role);
+        user = userService.create(user);
+        userService.delete(user.getId());
+        assertNull(userService.readById(user.getId()));
+    }
     @Test
     @Transactional
     @DirtiesContext
