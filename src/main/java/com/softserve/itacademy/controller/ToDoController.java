c9553d7c52797bc52cb04181aa63dd9dcf770d2b
learningGuru <nazar.furdychka50@gmail.com>
2021-01-19 17:49:36 +0200

    working on todos

--- src/main/java/com/softserve/itacademy/controller/ToDoController.java
+++ src/main/java/com/softserve/itacademy/controller/ToDoController.java
@@ -1,11 +1,46 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.service.impl.ToDoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/todos")
 public class ToDoController {
+
+    class Counter {
+        private int counter = 0;
+
+        Counter() {
+
+        }
+
+        private void count() {
+            counter++;
+
+
+        }
+
+        public int getCounter() {
+            this.count();
+            return counter;
+        }
+
+    }
+
+    @Autowired
+    ToDoServiceImpl toDoService;
+
+    @GetMapping("/")
+    public String todosPage(Model model) {
+        int counter = 0;
+        model.addAttribute("todos", toDoService.getAll());
+        model.addAttribute("counter", new Counter());
+        return "todo-lists";
+    }
 
     //add needed fields
 
